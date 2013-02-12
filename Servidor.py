1bfe6af818b50ee5ee167d9267c79fafc3a920b0
eddypre <eddypre@hotmail.com>
2013-02-12 10:47:55 -0600

    Update Servidor.py
    Servidor completo, escucha los IP request de los clientes y les responde con una dirección IP

--- Servidor.py
+++ Servidor.py
@@ -1,4 +1,4 @@
-'''  
+'''
 LABORATORIO 2
 
 Diseniar, implementar, comprobar experimentalmente y
@@ -8,20 +8,58 @@ Multiples clientes, un servidor
 Cada quien define que en si es el proposito de su
 protocolo
 Python, C, Ruby o Java de preferencia
-'''
 
+Simulacion de DHCP
+'''
 import socket
 import time
-#time.sleep(3)
+import sys
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "bla"
- 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
- 
+ip = "127.0.0.1"
+puerto = 9999
+
+try:
+	miSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	print "El socket se ha creado \o/"
+		
+except socket.error, mensaje: #Por si no se crea el socket
+	print "Error al crear socket: " + str(mensaje[0]) + " " + mensaje[1]
+	sys.exit() #Cerrar el programa
+
+#union de socket con la ip loopback(local) y el puerto 
+try:
+	miSocket.bind((ip, puerto))
+except socket.error, mens: #Por si hay error en la union
+	print "Union fallada. Codigo de error:" + str(mens[0]) + " " + mens[1]
+	sys.exit() #Cerrar el programa
+print "La union se ha completado..."
+
+#Lo siguiente es para recibir peticiones de los clientes y enviarles ip a cada uno
+
+#Direcciones de clase C (Comunmnete las que entrega Telmex)
+estatico = "192.168.1."
+ultimoOcteto = 250
+
 while True:
-	data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-	print "received message:", data
-	sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+	#Recibir mensaje del cliente con parametros (mensaje, ip)
+	informacion = miSocket.recvfrom(1024)
+	informacion1 = informacion[0]
+	informacion2 = informacion[1]
+
+	if not informacion: #Por si no le llega info al servidor
+		break
+
+	if ultimoOcteto < 255: #Para enviar direcciones ip que esten dentro del rango, osea menos de 255(que es la direccion de broadcast, en caso de mascara /24)
+		direccionEnString = str(ultimoOcteto)	
+		enviar = estatico + direccionEnString
+		miSocket.sendto(enviar, informacion2)
+		print "Envie este mensaje: ", enviar
+		ultimoOcteto = ultimoOcteto + 1
+	else:
+		MensajeAlerta = "Ya no es posible mandar mas direcciones, el pool esta lleno"
+		miSocket.sendto(MensajeAlerta, informacion2) #Enviar al cliente que envio su IP request un mensaje que ya no hay IP disponibles
+	
+	
+
+miSocket.close()
